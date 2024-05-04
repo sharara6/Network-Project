ed7e0194faf31ae3366a7780607dbe083a9ca627
Faris Sharara <122405756+sharara6@users.noreply.github.com>
2024-05-04 16:12:45 +0300

    Added Header's Size

--- Client.py
+++ Client.py
@@ -1,8 +1,10 @@
+HEADER = 64
+
 from socket import*
 
 
 with socket(AF_INET, SOCK_DGRAM) as client:
-    message = input("Type your Message")
+    message = input("Type your Message : ")
     client.sendto(message.encode(), (gethostname(), 8888))
     newMessage, adr = client.recvfrom(1024)
     print(newMessage.decode())
--- Server.py
+++ Server.py
@@ -1,3 +1,5 @@
+HEADER = 32
+
 import socket
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server:
@@ -5,4 +7,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server:
     while True:
         message, address = server.recvfrom(1024)
         newMessage = message.decode().upper()
+        newMessageSize = len(newMessage)
+        newMessage = f'{newMessageSize:<{HEADER - len(str(newMessageSize))}}{newMessage}'
         server.sendto(newMessage.encode(), address)
